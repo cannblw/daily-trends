4034bba795d601200d6affdb794a5539d62f0d33
cannblw <edgar.95.12@gmail.com>
2019-10-18 20:12:28 +0200

    Use data from service on News component

--- frontend/src/app/pages/news/news.component.ts
+++ frontend/src/app/pages/news/news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FeedService } from 'src/app/services/feed.service';
+import { Feed } from 'src/app/models/feed';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  feed: Feed[];
+
+  constructor(
+    private feedService: FeedService
+  ) { }
 
   ngOnInit() {
+    this.feedService.getFeed().subscribe(feed => this.feed = feed);
   }
 
 }
