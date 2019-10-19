dc893dfe6c67465e330fc5c2c28493c42b2892ba
cannblw <edgar.95.12@gmail.com>
2019-10-19 18:42:40 +0200

    Send CORS headers from backend to allow all origins

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
+ * This shouldn't be done in production. We're permitting
+ * access from all sources to allow Angular to access
+ * our API during development
+ */
+app.use(function (_, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use('/feed', feedRouter);
 
 module.exports = app;
