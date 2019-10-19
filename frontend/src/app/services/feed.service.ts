ca617bab28e1c7c881f120fbc2dd66f6d989d3fb
cannblw <edgar.95.12@gmail.com>
2019-10-19 18:43:38 +0200

    Modify feed service to perform an actual HTTP request to backend

--- frontend/src/app/services/feed.service.ts
+++ frontend/src/app/services/feed.service.ts
@@ -1,31 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
+import { BaseService } from './base.service';
 import { Feed } from '../models/feed';
-import { Publisher } from '../models/publisher';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class FeedService {
+export class FeedService extends BaseService {
+
+  constructor(
+    private http: HttpClient
+  ) {
+    super();
+  }
 
   getFeed(): Observable<Feed[]> {
-    const feedList: Feed[] = [];
-
-    const feed: Feed = {
-      title: 'SAMPLE TITLE',
-      body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut odio vehicula, lacinia urna at, lacinia ni
-        bh. Proin venenatis volutpat tortor, a pellentesque justo commodo ac. Class aptent taciti sociosqu ad litor
-        a torquent per conubia nostra, per inceptos himenaeos. Morbi accumsan at purus suscipit rhoncus. Vestibulum eget cursus erat, ne
-        c posuere dolor. In ultrices eu quam ut malesuada. Aliquam eu massa rutrum, volutpat leo laoreet, cursus turpis. Pellen
-        tesque interdum tincidunt molestie.`,
-      image: 'http://via.placeholder.com/640x360',
-      source: 'http://samplesourceurl.com/source',
-      publisher: Publisher.EL_MUNDO,
-    };
-
-    for (let i = 0; i < 10; ++i) {
-      feedList.push(feed);
-    }
-
-    return of(feedList);
+    return this.http.get<Feed[]>(`${this.getBaseUrl()}/feed`);
   }
 }
