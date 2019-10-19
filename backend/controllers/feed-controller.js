c70094e9f94ac3e71219c9fbcfe82b506bd23858
cannblw <edgar.95.12@gmail.com>
2019-10-19 18:23:24 +0200

    Map server items to desired format

--- backend/controllers/feed-controller.js
+++ backend/controllers/feed-controller.js
@@ -2,10 +2,19 @@
 const feedService = require('../services/feed-service');
 const logger = require('../services/logger-service');
 
+const FeedResponseDto = require('../models/dto/feed-response-dto');
+
 class FeedController {
   async findAll() {
     logger.i('Retrieving feed');
-    return feedService.findAll();
+
+    const serverFeed = await feedService.findAll();
+    
+    const responseFeed = serverFeed.map(item => {
+      return FeedResponseDto.fromServerItem(item);
+    });
+
+    return responseFeed;
   }
 
 } 
