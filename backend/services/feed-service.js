62dc37875cfb980ca66954fb106d6b64d890211a
cannblw <edgar.95.12@gmail.com>
2019-10-19 18:15:06 +0200

    Add Publisher to each element of the feed

--- backend/services/feed-service.js
+++ backend/services/feed-service.js
@@ -3,14 +3,22 @@ const Parser = require('rss-parser');
 const parser = new Parser();
 
 const environment = require('../environment');
+const Publisher = require('../models/publisher');
 
 class FeedService {
   async findAll() {
     const elMundoFeed = await this.findElMundoFeed();
     const elPaisFeed = await this.findElPaisFeed();
 
-    const elMundoItems = elMundoFeed.items;
-    const elPaisItems = elPaisFeed.items;
+    const elMundoItems = elMundoFeed.items.map(item => {
+      item.publisher = Publisher.EL_MUNDO;
+      return item;
+    });
+
+    const elPaisItems = elPaisFeed.items.map(item => {
+      item.publisher = Publisher.EL_PAIS;
+      return item;
+    });
 
     const feed = [...elMundoItems, ...elPaisItems];
 
