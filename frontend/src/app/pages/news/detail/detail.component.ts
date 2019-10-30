8c86c53beb61c8dcfbad6c5a6cf090e077d45ef8
cannblw <edgar.95.12@gmail.com>
2019-10-30 18:13:05 +0100

    Finish detail page

--- frontend/src/app/pages/news/detail/detail.component.ts
+++ frontend/src/app/pages/news/detail/detail.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { FeedService } from 'src/app/services/feed.service';
 import { Feed } from 'src/app/models/feed';
-import { PublisherName } from 'src/app/models/publisher';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-news-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class NewsDetailComponent implements OnInit {
+export class NewsDetailComponent implements OnInit, OnDestroy {
+
+  newsDetailSubscription: Subscription;
+  news: Feed;
 
   constructor(
-
+    private route: ActivatedRoute,
+    private feedService: FeedService
   ) { }
 
   ngOnInit() {
+    const newsId = this.route.snapshot.params.id;
 
+    if (newsId) {
+      this.newsDetailSubscription = this.feedService.findById(+newsId).subscribe(news => {
+        this.news = news;
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this.newsDetailSubscription.unsubscribe();
   }
 }
